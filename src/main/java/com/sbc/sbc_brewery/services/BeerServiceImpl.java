cb3ea95a80e64e23b9060cf8d9ce2303576ce78a
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-30 11:02:14 +0100

    Beer put and delete mapping

--- src/main/java/com/sbc/sbc_brewery/services/BeerServiceImpl.java
+++ src/main/java/com/sbc/sbc_brewery/services/BeerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.sbc.sbc_brewery.services;
 
 import com.sbc.sbc_brewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -16,9 +18,21 @@ public class BeerServiceImpl implements BeerService {
 
     @Override
     public BeerDto saveNewBeer(BeerDto beerDto) {
+        //todo add real saving of new beer
         return BeerDto.builder()
                 .id(UUID.randomUUID())
                 .build();
     }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+    //todo add real impl for beer update
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting beer with id {}", beerId);
+    }
+
 
 }
--- src/main/java/com/sbc/sbc_brewery/web/controller/BeerController.java
+++ src/main/java/com/sbc/sbc_brewery/web/controller/BeerController.java
@@ -25,11 +25,25 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto){
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto){
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
         return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handlePut(@PathVariable("beerId") UUID beerId,@RequestBody BeerDto beerDto){
+
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{beerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete(@PathVariable("beerId") UUID beerId){
+        beerService.deleteById(beerId);
     }
 }
