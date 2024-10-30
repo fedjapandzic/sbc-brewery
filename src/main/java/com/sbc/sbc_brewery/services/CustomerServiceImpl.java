52b9884ae67fed1b01b00d1c526e3c3b1bdbce6e
Fedja Pandzic <fedjapandzic1@gmail.com>
2024-10-30 11:09:38 +0100

    Customer all CRUD operations(controller side)

--- src/main/java/com/sbc/sbc_brewery/services/CustomerServiceImpl.java
+++ src/main/java/com/sbc/sbc_brewery/services/CustomerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.sbc.sbc_brewery.services;
 
 import com.sbc.sbc_brewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService {
     @Override
@@ -11,5 +13,22 @@ public class CustomerServiceImpl implements CustomerService {
         return CustomerDto.builder().id(UUID.randomUUID())
                 .name("Fedja")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo add real impl for customer update
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+        log.info("Deleting customer with id {}", customerId);
     }
 }
